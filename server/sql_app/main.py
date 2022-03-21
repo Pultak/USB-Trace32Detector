d446b62ca8beb80b96c8ab51e0dec0fb8656c98f
matejzeman <zeman339@gmail.com>
2022-03-21 23:57:18 +0100

    re #9304 HelloWorld API for sending data defined in USBLog class. Returns simple sucess message

--- server/sql_app/main.py
+++ server/sql_app/main.py
@@ -1,57 +1,28 @@
-from typing import List
-
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-
-from . import crud, models, schemas
-from .database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
-
+from fastapi import FastAPI
+import fastapi
+from pydantic import BaseModel
+from typing import Optional, List
 
 app = FastAPI()
-    
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
+class Device(BaseModel):
+	vendor_id: int
+	product_id: int
+	serial_number: int
 
-@app.post("/device/", response_model=schemas.Device)
-def create_device(device: schemas.DeviceCreate, db: Session = Depends(get_db)):
-    return crud.create_device(db=db, device=device)
+class USBLog(BaseModel):
+	username: str
+	hostname: str
+	timestamp: str
+	device: Device
+	status:str
 
-@app.get("/devices/", response_model=List[schemas.Device])
-def read_devices(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    devices = crud.get_devices(db, skip=skip, limit=limit)
-    return devices
-
-
-@app.get("/device/{device_id}", response_model=schemas.Device)
-def read_device(device_id: int, db: Session = Depends(get_db)):
-    db_device = crud.get_device(db, device_id=device_id)
-    if db_device is None:
-        raise HTTPException(status_code=404, detail="Device not found")
-    return db_device
-
-
-@app.post("/api/v1/logs/{device_id}/", response_model=schemas.USBLog)
-def create_device_logs(device_id: int, log: schemas.USBLogCreate, db: Session = Depends(get_db)):
-    return crud.create_device_logs(db=db, item=log, dev_id=device_id)
-
-
-@app.get("/logs/", response_model=List[schemas.USBLog])
-def read_logs(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_logs(db, skip=skip, limit=limit)
-    return items
-
-@app.get("/logs/{device_id}", response_model=List[schemas.USBLog])
-def read_log(device_id: int, db: Session = Depends(get_db)):
-    db_log = crud.get_log(db, device_id=device_id)
-    if db_log is None:
-        raise HTTPException(status_code=404, detail="Logs not found")
-    return db_log
-
+@app.post("/api/v1/usb-logs/")
+async def root_post(usb_log: USBLog):
+	print(usb_log)
+	return {"message": "Sucess"}
+	
+@app.get("/")
+async def root_read():
+	print("Hello")
+	return {"message":"Hello World"}
