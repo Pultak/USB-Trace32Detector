c318a92c20ec5fa5b44d1f1a6837108b8cca3c9f
Pultak <pultak5@gmail.com>
2022-04-01 20:52:04 +0200

    re #9437 initial lauterbach debugger client structure setup

---
+++ ld_client/LDClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Program {
+
+    // Main Method
+    static public void Main() {
+
+        Console.WriteLine("Main Method");
+    }
+}
---
+++ ld_client/LDClientTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace LDClientTests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
