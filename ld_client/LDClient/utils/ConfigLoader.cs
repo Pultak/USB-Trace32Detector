58cf6a6f304a0b4de9ce5529e788b1ca69411094
Pultak <pultak5@gmail.com>
2022-04-09 22:12:50 +0200

    re #9438 Added api configuration values

--- ld_client/LDClient/utils/ConfigLoader.cs
+++ ld_client/LDClient/utils/ConfigLoader.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 namespace LDClient.utils {
     internal class ConfigLoader {
         private const string LoggingSection = "Logging";
+        private const string NetworkSection = "Network";
 
 
         public int LogChunkSize { get; set; }
@@ -15,6 +16,14 @@ namespace LDClient.utils {
         public LogVerbosity LogVerbosityType { get; set; } = LogVerbosity.Full;
 
         public LogFlow LogFlowType { get; set; } = LogFlow.Console;
+
+        public string ApiBaseAddress { get; set; }
+        public string ApiUsbEndPoint { get; set; }
+        public uint ApiPort { get; set; }
+        public uint ApiRetryPeriod { get; set; }
+
+        public string DebuggerAddress { get; set; }
+        public int DebuggerPort { get; set; }
 
         public ConfigLoader() {
             var configuration = new ConfigurationBuilder()
@@ -34,7 +43,16 @@ namespace LDClient.utils {
                 LogCleanupPeriod = int.Parse(logging["LogCleanupPeriod"]);
                 LogFlowType = (LogFlow)int.Parse(logging["LogFlowType"]);
                 LogVerbosityType = (LogVerbosity)int.Parse(logging["LogVerbosityType"]);
+                
+                var network = configuration.GetSection(NetworkSection);
+                ApiBaseAddress = network["ApiBaseAddress"];
+                ApiUsbEndPoint = network["ApiLDEndPoint"];
+                ApiPort = uint.Parse(network["ApiPort"]);
+                ApiRetryPeriod = uint.Parse(network["ApiRetryPeriod"]);
 
+
+                DebuggerAddress = network["DebuggerAddress"];
+                DebuggerPort = int.Parse(network["DebuggerPort"]);
 
                 Console.WriteLine("Configuration successfully loaded!");
             } catch (FormatException e) {
