f5a08dd55a0848dc146f6f9d70ebfbd415daf981
Pultak <pultak5@gmail.com>
2022-04-10 23:02:09 +0200

    re #9433 detection definition in Main

--- ld_client/LDClient/Program.cs
+++ ld_client/LDClient/Program.cs
@@ -1,8 +1,11 @@
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using LDClient.detection;
 using LDClient.network;
 using LDClient.utils;
 using LDClient.utils.loggers;
 
-namespace LDClient; 
+namespace LDClient;
 
 internal class Program {
 
@@ -13,25 +16,49 @@ internal class Program {
         Config.ApiPort, Config.ApiUsbEndPoint, Config.RetryPeriod, Config.MaxEntries,
         Config.MaxRetries, Config.CacheFileName);
 
+
+    private static NetworkDetection _netDetect = new(Config.ApiPort, Config.DetectionPeriod);
+    private static ProcessDetection _procDetect = new(Config.T32ProcessName, Config.DetectionPeriod);
+
+
     // Main Method
     public static async Task Main() {
-
         var apiClientThread = new Thread(DefaultApiClient.Run) {
             IsBackground = true
         };
+        DefaultLogger.Debug("Main -> starting the ApiClient");
         apiClientThread.Start();
 
-        DefaultLogger.Debug("Main -> starting SendPayloadAsync");
-        await DefaultApiClient.SendPayloadAsync(ApiClient.ExampleInfo);
+        var admin = IsAdministrator();
+        DefaultLogger.Debug($"Is program executed with admin rights? {admin}");
 
+        var networkTread = new Thread(_netDetect.RunPeriodicDetection);
+        networkTread.Start();
+        if (admin) {
+            _procDetect.RegisterProcessListeners();
 
-        DefaultLogger.Debug("Main -> lets slack for a bit");
-        Thread.Sleep(30000);
+        } else {
+            var processThread = new Thread(_procDetect.RunPeriodicDetection);
+            processThread.Start();
+            processThread.Join();
+        }
+
+        networkTread.Join();
 
         DefaultLogger.Debug("Main -> stopping the ApiClient");
         DefaultApiClient.Stop();
         apiClientThread.Join();
         DefaultLogger.Debug("Main -> finished");
     }
+
+    public static bool IsAdministrator() {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+        } else {
+            return false;
+        }
     }
 }
