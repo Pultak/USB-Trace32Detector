4056867185713b32bf144b32a6a17cff78e961a5
Pultak <pultak5@gmail.com>
2022-04-10 23:00:05 +0200

    re #9433 Abstract detection definition added

---
+++ ld_client/LDClient/detection/IDetection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDClient.detection {
+    internal interface IDetection {
+
+        public void DetectAsync();
+
+        public void RunPeriodicDetection();
+        public void StopPeriodicDetection();
+    }
+}
