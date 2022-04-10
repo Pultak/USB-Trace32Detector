eda1e8c74a34dde8184b960627c82cc7083d482d
Pultak <pultak5@gmail.com>
2022-04-10 19:59:33 +0200

    re #9441 Added core of cache manager

--- ld_client/LDClient/network/ApiClient.cs
+++ ld_client/LDClient/network/ApiClient.cs
@@ -94,6 +94,45 @@ namespace LDClient.network {
                                  $"Request body: {json},\n" +
                                  $"Response: {JsonConvert.SerializeObject(responseToLog)}");
 
+        private async Task ResendPayloadsAsync() {
+            var numberOfPayloadsToResend = Math.Min(_maxRetries, _cache.EstimatedCountOfItemsInQueue);
+            var payloads = new List<Payload>();
+
+            using (var session = _cache.OpenSession()) {
+                for (var i = 0; i < numberOfPayloadsToResend; i++) {
+                    var rawBytes = session.Dequeue();
+                    var payload = JsonSerializer.Deserialize<Payload>(rawBytes);
+                    if (payload is not null) {
+                        payloads.Add(payload);
+                    }
+                }
+                session.Flush();
+            }
+
+            if (payloads.Count > 0) {
+                Program.DefaultLogger.Debug($"ResendPayloadAsync -> {payloads.Count} unsent payloads");
+                var tasks = new List<Task>();
+                foreach (var payload in payloads) {
+                    Program.DefaultLogger.Info($"Resending {payload}.");
+                    tasks.Add(SendPayloadAsync(payload));
+                }
+                await Task.WhenAll(tasks);
+            }
+        }
+
+
+        private void CachePayload(Payload payload) {
+            Program.DefaultLogger.Info($"Storing {payload} into the cache.");
+            var numberOfCachedPayloads = _cache.EstimatedCountOfItemsInQueue;
+            using var session = _cache.OpenSession();
+            if (numberOfCachedPayloads >= _maxEntries) {
+                session.Dequeue();
+            }
+            var payloadJson = JsonSerializer.Serialize(payload);
+            session.Enqueue(Encoding.UTF8.GetBytes(payloadJson));
+            session.Flush();
+        }
+
         public async void Run() {
             Program.DefaultLogger.Info("Api Client thread has started");
             _running = true;
