31b6e8b723d1e34724104be7978ca00452d89207
silhavyj <jakub.sil@seznam.cz>
2022-05-05 21:06:17 +0200

    re #9570 Commented Program.cs

--- ld_client/LDClient/Program.cs
+++ ld_client/LDClient/Program.cs
@@ -9,14 +9,34 @@ using static System.Reflection.Assembly;
 
 namespace LDClient;
 
+/// <summary>
+/// This class represents the main class of the application.
+/// </summary>
 internal static class Program {
 
+    /// <summary>
+    /// Sleep period of the main thread of the application.
+    /// </summary>
     private const int MainLoopDelayMs = 30000; 
 
+    /// <summary>
+    /// Instance of a config loader.
+    /// </summary>
     public static ConfigLoader Config { get; } = new();
+    
+    /// <summary>
+    /// Default logger used throughout the application.
+    /// </summary>
     public static ALogger DefaultLogger { get; } = ALogger.Current;
+    
+    /// <summary>
+    /// Instance of an API client.
+    /// </summary>
     private static IApiClient? DefaultApiClient { get; set; }
     
+    /// <summary>
+    /// Instance of an info fetcher.
+    /// </summary>
     private static readonly InfoFetcher InfoFetcher = new(
         Config.FetchInfoMaxAttempts,
         Config.FetchInfoAttemptPeriod,
@@ -27,12 +47,19 @@ internal static class Program {
         Config.T32RemWaitTimeoutMs
     );
     
+    /// <summary>
+    /// The main entry pint of the application.
+    /// </summary>
+    /// <returns>1 if something goes wrong, 0 otherwise.</returns>
     public static int Main() {
+        // Make sure that there is only one running instance of this application.
         if (GetProcessesByName(Path.GetFileNameWithoutExtension(GetEntryAssembly()?.Location)).Length > 1) {
             DefaultLogger.Error("Another instance of the application is already running");
             return 1;
         }
         
+        // Create an instance of an API client with all
+        // the appropriate parameters defined in the config file.
         DefaultApiClient = new ApiClient(
             Config.ApiBaseAddress,
             Config.ApiPort, 
@@ -42,6 +69,7 @@ internal static class Program {
             new PersistentQueue(Config.CacheFileName)
         );
         
+        // Create a new process detector.
         IProcessDetection processProcessDetection = new ProcessDetection(
             Config.T32ProcessName,
             Config.DetectionPeriod,
@@ -50,18 +78,26 @@ internal static class Program {
             new ProcessUtils()
         );
         
+        // Create and start a new thread that periodically
+        // resends filed payloads to the server.
         var apiClientThread = new Thread(DefaultApiClient.Run) {
             IsBackground = true
         };
         apiClientThread.Start();
 
+        // Create and start a new thread that periodically checks
+        // if the desired process is currently running or not.
         var processThread = new Thread(processProcessDetection.RunPeriodicDetection) {
             IsBackground = true
         };
         processThread.Start();
 
+        // The main thread does "nothing"
         while (true) {
             Thread.Sleep(MainLoopDelayMs);
         }
+
+        // The execution of the program should never reach this point.
+        return 0;
     }
 }
