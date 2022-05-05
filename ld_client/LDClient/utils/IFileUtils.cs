9657d7e0e156d39c8deea6a2b318e894689255c5
silhavyj <jakub.sil@seznam.cz>
2022-05-05 20:57:48 +0200

    re #9570 Commented ConfigLoader.cs, FileUtils.cs, and IFileUtils.cs

--- ld_client/LDClient/utils/IFileUtils.cs
+++ ld_client/LDClient/utils/IFileUtils.cs
@@ -5,8 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LDClient.utils {
+    
+    /// <summary>
+    /// This interface defines IO operations.
+    /// </summary>
     public interface IFileUtils {
 
+        /// <summary>
+        /// Reads all lines of a files and returns them as a array.
+        /// </summary>
+        /// <param name="file">path to the file</param>
+        /// <returns>all the lines of the file (as an array)</returns>
         public string[] ReadFileAllLines(string file);
     }
 }
