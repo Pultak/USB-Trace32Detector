a155a76350ed6c6804382370a8c7acfab1cf18f1
silhavyj <jakub.sil@seznam.cz>
2022-05-05 18:55:11 +0200

    re #9570 Commented HttpClient.cs, IHttpClient.cs, and IApiClient.cs

--- ld_client/LDClient/network/IHttpClient.cs
+++ ld_client/LDClient/network/IHttpClient.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 using LDClient.network.data;
 
 namespace LDClient.network {
+    
+    /// <summary>
+    /// This interface defines the functionality of a HTTP client
+    /// through which the API client sends data (payloads) to the server.
+    /// </summary>
     public interface IHttpClient {
 
+        /// <summary>
+        /// Asynchronically sends data in JSON format to the server.
+        /// </summary>
+        /// <param name="payload">Payload to be sent to the server</param>
+        /// <returns></returns>
         public Task<HttpResponseMessage> PostAsJsonAsync(Payload payload);
     }
 }
