625c7c2ed9d2c1b536120b6363bcfb3989414a2d
silhavyj <jakub.sil@seznam.cz>
2022-05-05 19:04:07 +0200

    re #9570 Commented ConnectionStatus.cs, DebuggerInfo.cs, and Payload.cs

--- ld_client/LDClient/network/data/ConnectionStatus.cs
+++ ld_client/LDClient/network/data/ConnectionStatus.cs
@@ -2,10 +2,21 @@
 
 namespace LDClient.network.data {
     
+    /// <summary>
+    /// This enumeration defines different states in
+    /// which a debugger can be.
+    /// </summary>
     public enum ConnectionStatus {
         
+        /// <summary>
+        /// Debugger is connected
+        /// </summary>
         [EnumMember(Value = "connected")]
         Connected,
+        
+        /// <summary>
+        /// Debugger is disconnected
+        /// </summary>
         [EnumMember(Value = "disconnected")]
         Disconnected
     }
