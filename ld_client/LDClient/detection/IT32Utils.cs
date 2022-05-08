68087f99f168fb52dded94470ac3d67d582e31eb
Pultak <pultak5@gmail.com>
2022-05-08 12:49:38 +0200

    re #9712 restructuring of info fetcher

---
+++ ld_client/LDClient/detection/IT32Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDClient.detection {
+    public interface IT32Utils {
+
+        public bool InitConnection();
+        public bool ExecuteCommands();
+        public bool CloseConnection();
+
+    }
+}
