6cde905ffbe816a629dfad1019221d2657874270
Pultak <pultak5@gmail.com>
2022-05-08 12:50:38 +0200

    re #9712 Added t32 api fetcher

---
+++ ld_client/LDClient/detection/T32ApiFetcher.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDClient.detection {
+
+    public class T32ApiFetcher : AInfoFetcher, IT32Utils {
+#if _WINDOWS
+        private const string T32DllLibrary = "./lib/t32api64.dll";
+#else
+        private const string T32DllLibrary = "./lib/t32api64.so";
+#endif
+
+        private readonly string _t32Address;
+        private readonly string _t32Port;
+        private readonly string _t32PacketLength;
+        private readonly string[] _commands;
+
+        public T32ApiFetcher(uint maxAttempts, uint waitPeriodMs, string infoFilePath, string t32Address, 
+            string t32Port, string t32PacketLength, string[] commands) : base(maxAttempts, waitPeriodMs, infoFilePath) {
+            this._t32Address = t32Address;
+            this._t32Port = t32Port;
+            this._t32PacketLength = t32PacketLength;
+            this._commands = commands;
+        }
+
+
+        [DllImport(T32DllLibrary, CharSet = CharSet.Ansi)]
+        private static extern int T32_Config(string s1, string s2);
+
+        [DllImport(T32DllLibrary, CharSet = CharSet.Ansi)]
+        private static extern int T32_Init();
+
+        [DllImport(T32DllLibrary, CharSet = CharSet.Ansi)]
+        private static extern int T32_Attach(int dev);
+
+        [DllImport(T32DllLibrary, CharSet = CharSet.Ansi)]
+        private static extern int T32_Cmd(string command);
+
+        [DllImport(T32DllLibrary, CharSet = CharSet.Ansi)]
+        private static extern int T32_Exit();
+        
+        public bool InitConnection() {
+            Program.DefaultLogger.Debug("Trace32 connection initialization");
+            var config1 = T32_Config("NODE=", _t32Address);
+            var config2 = T32_Config("PORT=", _t32Port);
+            var config3 = T32_Config("PACKLEN=", _t32PacketLength);
+
+            if (config1 != 0 || config2 != 0 || config3 != 0) {
+                Program.DefaultLogger.Error("Trace32 API connection configuration failed.");
+                return false;
+            }
+            var init = T32_Init();
+            if (init != 0) {
+                Program.DefaultLogger.Error("Trace32 API connection init failed.");
+                return false;
+            }
+
+            var attach = T32_Attach(1);
+            if (attach != 0) {
+                Program.DefaultLogger.Error("Trace32 API connection attach failed.");
+            }
+
+            Program.DefaultLogger.Info("Trace32 connection established");
+            return true;
+        }
+
+        public bool ExecuteCommands() {
+            Program.DefaultLogger.Info("Trace32 API commands execution.");
+            foreach (var command in _commands) {
+                Program.DefaultLogger.Debug($"Executing Trace32 command '{command}'.");
+                var ret = T32_Cmd(command);
+                if (ret != 0) {
+                    Program.DefaultLogger.Error($"Execution of command '{command}' failed. Return code {ret}.");
+                    return false;
+                }
+            }
+            Program.DefaultLogger.Info("All Trace32 commands executed successfully.");
+            return true;
+        }
+
+        public bool CloseConnection() {
+            Program.DefaultLogger.Debug("Trace32 connection exit");
+            return T32_Exit() == 0;
+        }
+
+        protected override bool FetchData() {
+            var connected = false;
+            for (var i = 0; i < _maxAttempts; i++) {
+                connected = InitConnection();
+                if (connected) {
+                    break;
+                }
+                Thread.Sleep((int)_waitPeriodMs);
+
+            }
+            return connected && ExecuteCommands() && CloseConnection();
+        }
+    }
+}
---
+++ ld_client/LDClient/detection/T32RemFetcher.cs
@@ -0,0 +1,72 @@
+using System.Diagnostics;
+using LDClient.utils;
+using LDClient.utils.loggers;
+
+namespace LDClient.detection {
+
+    public class T32RemFetcher : AInfoFetcher{
+
+        /// <summary>
+        /// Path to the t32rem.exe file which is used to send commands to the debugger.
+        /// </summary>
+        private readonly string _f32RemExecutable;
+
+        /// <summary>
+        /// Arguments (commands) sent to the debugger in order to generate a .txt file
+        /// containing all the desired information.
+        /// </summary>
+        private readonly string[] _f32RemArguments;
+
+        /// <summary>
+        /// Status code indicating a successful termination of t32rem.exe
+        /// </summary>
+        private readonly int _f32SuccessExitCode;
+
+
+        /// <summary>
+        /// Timeout used when waiting for the t32rem.exe to finish.
+        /// </summary>
+        private readonly int _f32WaitTimeoutMs;
+
+        /// <summary>
+        /// Instance of ProcessUtils which encapsulates common functionality
+        /// when it comes to dealing with processes (limited by the needs of this application).
+        /// </summary>
+        public IProcessUtils ProcessUtils;
+
+        /// <summary>
+        /// Creates an instance of this class.
+        /// </summary>
+        /// <param name="maxAttempts">Maximum number of attempts to locate and parse the .txt file</param>
+        /// <param name="waitPeriodMs">Period (how often) the application tries to locate and parse the .txt file</param>
+        /// <param name="infoFilePath">Path to the .txt file which is generated from the debugger</param>
+        /// <param name="f32RemExecutable">Path to the t32rem.exe file which is used to send commands to the debugger</param>
+        /// <param name="f32RemArguments">Arguments (commands) sent to the debugger in order to generate a .txt file containing all the desired information.</param>
+        /// <param name="f32SuccessExitCode">Status code indicating a successful termination of t32rem.exe</param>
+        /// <param name="f32WaitTimeoutMs">Timeout used when waiting for the t32rem.exe to finish</param>
+
+        public T32RemFetcher(uint maxAttempts, uint waitPeriodMs, string infoFilePath, string f32RemExecutable,
+            string[] f32RemArguments, int f32SuccessExitCode, int f32WaitTimeoutMs) : base(maxAttempts, waitPeriodMs, infoFilePath) {
+            _f32RemExecutable = f32RemExecutable;
+            _f32RemArguments = f32RemArguments;
+            _f32SuccessExitCode = f32SuccessExitCode;
+            _f32WaitTimeoutMs = f32WaitTimeoutMs;
+            ProcessUtils = new ProcessUtils();
+
+        }
+
+        protected override bool FetchData() {
+            if (_f32RemArguments == null) {
+                Program.DefaultLogger.Error($"Failed to run {_f32RemExecutable} - no parameters were given");
+                return false;
+            }
+            // Execute one all arguments (commands) one by one.
+            foreach (var argument in _f32RemArguments) {
+                if (!ProcessUtils.ExecuteNewProcess(_f32RemExecutable, argument, _f32WaitTimeoutMs, _f32SuccessExitCode)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
